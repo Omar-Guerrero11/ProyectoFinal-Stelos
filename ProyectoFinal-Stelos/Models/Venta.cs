50a317df30f5baf500a8c5d37c6be76aa2d8b696
Diego <diego2805f@gmail.com>
2026-05-26 00:11:27 -0700

    Agregar modelos de venta, usuario, producto y detalleventa

---
+++ ProyectoFinal-Stelos/Models/Venta.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoFinal_Stelos.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string MetodoPago { get; set; } = string.Empty;
+        public decimal Total { get; set; }
+        public string Estado { get; set; } = string.Empty;
+
+        public List<DetalleVenta> DetallesVenta { get; set; } = new();
+    }
+}
